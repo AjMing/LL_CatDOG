5bb98ac2dc4a02dc0298b64cb803c04a3d9814d7
AjMing <62439174+AjMing@users.noreply.github.com>
2024-04-09 14:37:39 +0700

    Update LL_test.cpp

--- LL_test.cpp
+++ LL_test.cpp
@@ -4,27 +4,24 @@ using namespace std;
 #include"NODE.h"
 #include"cat_dog.h"
 #include"LL.h"
-/*1.Create a constructor & Destructor according to its type.   */
-
-/*4.Create a snap_finger (in LL class or in class inherited from LL) to randomly delete half of the node */
-
+void menu(); // Finish your menu to choose cat or dog.
 int main(int argc, char *argv[])
 {  LL A;
    int i;
    NODE *t;
 
-   for(i=1;i<argc;i++) {
-     /* modify to argument for cat or dog*/
-        if(i%2)   t=new cat;
+ while(i=menu()!=3){
+ /* modified main to either add cat or dog*/
+  
+   
+        if(i==1)   t=new cat;
          else  t=new dog;
          // t=new NODE(atoi(argv[i]));
-                       A.add_node(t);
-   }
+           A.add_node(t);
+ }
+   
    A.show_all();
 
-   //A.snap_finger(); // kill 1/2 (make size=0)
-
-   A.show_all();
 
     
 }
